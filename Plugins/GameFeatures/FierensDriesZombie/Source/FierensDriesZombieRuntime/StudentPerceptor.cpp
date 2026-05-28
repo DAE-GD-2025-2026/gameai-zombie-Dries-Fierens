b283f83908286803da6be8270d62a28217c566a8
Dries Fierens <77333871+Dries-Fierens@users.noreply.github.com>
2026-05-28 16:03:05 +0200

    Working on survivor decision making

--- Plugins/GameFeatures/FierensDriesZombie/Source/FierensDriesZombieRuntime/StudentPerceptor.cpp
+++ Plugins/GameFeatures/FierensDriesZombie/Source/FierensDriesZombieRuntime/StudentPerceptor.cpp
@@ -2,7 +2,19 @@
 
 
 #include "StudentPerceptor.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "AIController.h"
+#include "Engine/Engine.h"
 
+namespace
+{
+	const FName TargetEnemyKey(TEXT("TargetEnemy"));
+	const FName LastKnownEnemyLocationKey(TEXT("LastKnownEnemyLocation"));
+	const FName HasEnemyInSightKey(TEXT("HasEnemyInSight"));
+	const FName TargetItemKey(TEXT("TargetItem"));
+	const FName TargetHouseKey(TEXT("TargetHouse"));
+	const FName IsInPurgeDangerKey(TEXT("IsInPurgeDanger"));
+}
 
 UStudentPerceptor::UStudentPerceptor()
 {
@@ -21,6 +33,44 @@ void UStudentPerceptor::BeginPlay()
 
 void UStudentPerceptor::OnPerceptionUpdated(AActor* Actor, FAIStimulus Stimulus)
 {
-	GEngine->AddOnScreenDebugMessage(5, 1.f, FColor::Green, 
-	FString::Printf(TEXT("Saw Something!")));
+	if (Actor == nullptr)
+	{
+		return;
+	}
+
+	APawn* PawnOwner = Cast<APawn>(GetOwner());
+	if (PawnOwner == nullptr)
+	{
+		return;
+	}
+
+	AAIController* AIController = Cast<AAIController>(PawnOwner->GetController());
+	if (AIController == nullptr)
+	{
+		return;
+	}
+
+	UBlackboardComponent* Blackboard = AIController->GetBlackboardComponent();
+	if (Blackboard == nullptr)
+	{
+		return;
+	}
+
+	const bool bSuccessfullySensed = Stimulus.WasSuccessfullySensed();
+	
+	if (bSuccessfullySensed)
+	{
+		Blackboard->SetValueAsObject(TEXT("TargetEnemy"), Actor);
+		Blackboard->SetValueAsBool(TEXT("HasEnemyInSight"), true);
+		Blackboard->SetValueAsVector(TEXT("LastKnownEnemyLocation"), Actor->GetActorLocation());
+	}
+	
+	if (GEngine != nullptr)
+	{
+		GEngine->AddOnScreenDebugMessage(
+			5,
+			1.0f,
+			bSuccessfullySensed ? FColor::Green : FColor::Red,
+			FString::Printf(TEXT("Perception updated: %s"), *GetNameSafe(Actor)));
+	}
 }
