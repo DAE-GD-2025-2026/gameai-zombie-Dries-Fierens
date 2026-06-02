38a4eee845c460f3e6b9c72341ddeb714ca7d398
Dries Fierens <77333871+Dries-Fierens@users.noreply.github.com>
2026-06-02 21:58:48 +0200

    Added own wander behavior

---
+++ Plugins/GameFeatures/FierensDriesZombie/Source/FierensDriesZombieRuntime/BTT_WanderFierensDries.cpp
@@ -0,0 +1,36 @@
+﻿#include "BTT_WanderFierensDries.h"
+
+#include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+UBTT_WanderFierensDries::UBTT_WanderFierensDries()
+{
+	NodeName = TEXT("Wander");
+	MoveLocationKey.AddVectorFilter(this, GET_MEMBER_NAME_CHECKED(UBTT_WanderFierensDries, MoveLocationKey));
+	MoveLocationKey.SelectedKeyName = TEXT("MoveLocation");
+}
+
+EBTNodeResult::Type UBTT_WanderFierensDries::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	const AAIController* AIController = OwnerComp.GetAIOwner();
+	if (AIController == nullptr)
+	{
+		return EBTNodeResult::Failed;
+	}
+
+	APawn* ControlledPawn = AIController->GetPawn();
+	UBlackboardComponent* Blackboard = OwnerComp.GetBlackboardComponent();
+	if (ControlledPawn == nullptr || Blackboard == nullptr)
+	{
+		return EBTNodeResult::Failed;
+	}
+
+	FVector RandomDirection = FMath::VRand();
+	RandomDirection.Z = 0.0f;
+	RandomDirection.Normalize();
+
+	const FVector RandomLocation = ControlledPawn->GetActorLocation() + RandomDirection * FMath::FRandRange(0.0f, Radius);
+	Blackboard->SetValueAsVector(MoveLocationKey.SelectedKeyName, RandomLocation);
+
+	return EBTNodeResult::Succeeded;
+}
---
+++ Plugins/GameFeatures/FierensDriesZombie/Source/FierensDriesZombieRuntime/BTT_WanderFierensDries.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "BehaviorTree/BehaviorTreeTypes.h"
+#include "BTT_WanderFierensDries.generated.h"
+
+UCLASS()
+class FIERENSDRIESZOMBIERUNTIME_API UBTT_WanderFierensDries : public UBTTaskNode
+{
+	GENERATED_BODY()
+
+public:
+	UBTT_WanderFierensDries();
+	
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+
+protected:
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	FBlackboardKeySelector MoveLocationKey;
+
+	UPROPERTY(EditAnywhere, Category = "Wander", meta = (ClampMin = "0.0"))
+	float Radius = 1500.0f;
+};
